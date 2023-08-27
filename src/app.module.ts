addb07ff80d35fb0f2e2c99de5cc605770885ac8
Brandon <dev.brandon.neves@gmail.com>
2023-08-27 20:54:36 -0300

    feat: create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { MediasModule } from './medias/medias.module';
+import { PublicationsModule } from './publications/publications.module';
+
 
 @Module({
-  imports: [],
+  imports: [PostsModule, MediasModule, PublicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
